48a6082ad64de0b326cb7554b8a12f8872722831
Mario Rodeghiero <mario.rodeghiero@comeon.com>
2025-04-21 21:41:01 +0200

    [contract][xs]: create add_post method

--- contract/src/lib.rs
+++ contract/src/lib.rs
@@ -1,6 +1,6 @@
 use near_sdk::borsh::{self, BorshDeserialize, BorshSerialize};
 use near_sdk::store::UnorderedMap;
-use near_sdk::{AccountId, near_bindgen};
+use near_sdk::{AccountId, near_bindgen, env};
 use near_sdk::serde::{Deserialize, Serialize};
 
 
@@ -34,6 +34,31 @@ impl Default for SocialNetworking {
             likes_by_user_id: UnorderedMap::new(b"n"),
             posts_by_tag: UnorderedMap::new(b"o"),
         }
-    }
-    
+    }   
 }
+
+#[near_bindgen]
+impl SocialNetworking {
+    pub fn add_post(&mut self, title: String, description: String, tags: String, media: String) -> Post {
+
+        let tags_iterator = tags.split(",");
+        let mut tags = Vec::<String>::new();
+        for tag in tags_iterator {
+            tags.push(tag.to_string());
+        }
+
+        let post = Post {
+            id: self.number_of_posts,
+            title,
+            description,
+            tags: tags.clone(),
+            media,
+            users_who_liked: Vec::<AccountId>::new(),
+            owner_id: env::signer_account_id(), // add the signer account id
+        };
+        
+        self.posts.insert(post.id, post.clone());
+        self.number_of_posts += 1;
+        return post;
+    }
+} 
